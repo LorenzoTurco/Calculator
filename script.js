0c162f1f636b17592ad78d94061610fd54f7c279
LorenzoTurco <l.turco@se19.qmul.ac.uk>
2022-07-01 16:11:57 +0100

    added functionality for majority of buttons

--- script.js
+++ script.js
@@ -1,14 +1,75 @@
 const input = document.querySelector("#input-screen");
 
-const button1 = document.querySelector("#button1");
-
 const buttons = document.getElementsByTagName("button");
 console.log(buttons);
 
 const displayNum = (event) => {
-  input.value = input.value + event.target.value;
+  const pressedButton = event.target.value;
+
+  switch (true) {
+    case pressedButton == "AC":
+      input.value = "";
+      break;
+
+    case pressedButton == "+/-":
+      reverseNumber();
+      break;
+
+    case pressedButton == "=":
+      performOperation();
+      break;
+
+    default:
+      input.value = input.value + pressedButton;
+  }
 };
 
 Array.from(buttons).forEach((button) =>
   button.addEventListener("click", displayNum)
 );
+
+const performOperation = () => {
+  let expression = [];
+  let value = NaN;
+
+  switch (true) {
+    case input.value.includes("+"):
+      expression = input.value.split("+");
+      value = parseFloat(expression[0]) + parseFloat(expression[1]);
+      break;
+
+    case input.value.includes("-"):
+      expression = input.value.split("-");
+      value = parseFloat(expression[0]) - parseFloat(expression[1]);
+      break;
+
+    case input.value.includes("x"):
+      expression = input.value.split("x");
+      value = parseFloat(expression[0]) * parseFloat(expression[1]);
+      break;
+
+    case input.value.includes("/"):
+      expression = input.value.split("/");
+      value = parseFloat(expression[0]) / parseFloat(expression[1]);
+      break;
+
+    case input.value.includes("%"):
+      expression = input.value.split("%");
+      value = parseFloat((expression[0] / parseFloat(expression[1])) * 100);
+  }
+
+  input.value = value;
+};
+
+const reverseNumber = () => {
+  if (parseInt(input.value) > 0) {
+    console.log(input.value);
+    console.log(parseInt(input.value) - parseInt(input.value) * 2);
+    input.value = parseInt(input.value) - parseInt(input.value) * 2;
+    console.log(input.value);
+
+    return;
+  }
+  input.value = input.value * -1;
+  return;
+};
